dd34c1ad70b2cab45115eb6a98dfa5a9e8a64c5d
RadzPrower <anthony.johns.6@gmail.com>
2023-12-12 16:39:10 -0500

    Implemented Day 12 Part 2 (credit to Diderikdm of reddit for the concept)

--- Day12.py
+++ Day12.py
@@ -1,10 +1,11 @@
 from itertools import combinations
+from functools import cache
 
 
 def main(data):
     global spring_map
     spring_map = list(data)
-    possible_arrangements = find_arrangements(spring_map)
+    possible_arrangements = find_arrangements(spring_map, True)
     print(f"The sum of all possible arrangements is {sum(possible_arrangements)}.")
 
 
@@ -12,6 +13,8 @@ def find_arrangements(spring_map, folded=False):
     result = []
     if folded:
         expand_spring_map(spring_map)
+        result = find_recursively(spring_map)
+        return result
     for line in spring_map:
         ind_result = 0
         individual, groups = line.split(" ")
@@ -39,13 +42,62 @@ def find_arrangements(spring_map, folded=False):
                 temp_lengths.append(len(group))
             if groups != temp_lengths:
                 continue
-            for idx, char in enumerate(temp):
-                if char != individual[idx] and individual[idx] != "?":
-                    valid = False
+            for idx in range(0,len(temp),3):
+                valid = string_compare(temp[idx:idx + 3], individual[idx:idx + 3])
+                if not valid:
+                    break
+            #for idx, char in enumerate(temp):
+            #    if char != individual[idx] and individual[idx] != "?":
+            #        valid = False
             if valid:
                 ind_result += 1
         result.append(ind_result)
     return result
+
+
+def find_recursively(spring_map):
+    result = []
+    for line in spring_map:
+        if line == "...?#??#?#????#..?...?#??#?#????#..?...?#??#?#????#..?...?#??#?#????#..?...?#??#?#????#.. 10,1,10,1,10,1,10,1,10,1":
+            _ = 1
+        springs, groups = line.split(" ")
+        result.append(recurse(springs, groups))
+    return result
+
+@cache
+def recurse(remaining_springs, groups):
+    result = 0
+    if not groups:
+        return "#" not in remaining_springs
+    groups_int = [int(i) for i in groups.split(",")]
+    current_group = int(groups_int[0])
+    groups = ",".join(groups.split(",")[1:])
+    groups_int = groups_int[1:]
+    for i in range(len(remaining_springs) - sum(groups_int) - len(groups_int) - current_group + 1):
+        if "#" in remaining_springs[:i]:
+            break
+        if (nxt := i + current_group) <= len(remaining_springs) \
+                and "." not in remaining_springs[i : nxt] \
+                and remaining_springs[nxt : nxt + 1] != "#":
+            result += recurse(remaining_springs[nxt + 1:], groups)
+    return result
+
+
+@cache
+def string_compare(string1, string2):
+    result = True
+    for idx, char in enumerate(string1):
+        result = character_compare(char, string2[idx])
+        if result:
+            break
+    return not result
+
+
+@cache
+def character_compare(char1, char2):
+    if char1 != char2 and char2 != "?":
+        return True
+    return False
 
 
 def expand_spring_map(spring_map):
