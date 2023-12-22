ba45fe3917b5954df72bf1d94e14347f5fb24e85
RadzPrower <anthony.johns.6@gmail.com>
2023-12-22 12:50:19 -0500

    Attempt at Part 2 but not functional

--- Day21.py
+++ Day21.py
@@ -4,22 +4,40 @@ from functools import cache
 def main(data):
     global garden_map
     garden_map = data
-    steps = 10
-    fill_space(steps)
+    steps = 64
+    print(f"The elf could reach {fill_space(steps)} plots in {steps} steps")
 
     # Part 1
     # all_potential_tiles = find_all_potential_tiles(steps)
-    # print(f"The elf could reach {len(all_potential_tiles)} in {steps} steps.")
+    # print(f"The elf could reach {len(all_potential_tiles)} plots in {steps} steps.")
 
 
 def fill_space(steps):
+    result = 0
     center_x, center_y = find_start()
     start_x = center_x - steps
     end_x = center_x + steps + 1
+    y_expansion = 0
     for x in range(start_x, end_x):
         if x < center_x:
-            y_expansion = steps - center_x - x
-            breakpoint()
+            if x == center_x and y == center_y:
+                breakpoint()
+            for y in range(center_y - y_expansion, center_y + y_expansion + 1, 2):
+                x_translated = x % len(garden_map)
+                y_translated = y % len(garden_map[0])
+                if garden_map[x_translated][y_translated] != "#":
+                    result += 1
+            y_expansion += 1
+        if x >= center_x:
+            if x == center_x and y == center_y:
+                breakpoint()
+            for y in range(center_y - y_expansion, center_y + y_expansion + 1, 2):
+                x_translated = x % len(garden_map)
+                y_translated = y % len(garden_map[0])
+                if garden_map[x_translated][y_translated] != "#":
+                    result += 1
+            y_expansion -= 1
+    return result
 
 
 def find_all_potential_tiles(steps=64):
