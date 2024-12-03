2d55c627a2abcb4ebc95de09207382fa2c8fb429
RadzPrower <anthony.johns.6@gmail.com>
2024-12-03 08:28:00 -0500

    Implementation of solution for Day 3

--- Day03.py
+++ Day03.py
@@ -1,3 +1,25 @@
+import re
+
 def main(data):
-    print("This day is not yet implemented.")
-    return
+    result1 = parse_corrupted_data(data)
+    result = "The result of ALL multiply operations is " + str(result1) + "."
+    result2 = parse_corrupted_data(data, True)
+    result += "\nThe final result is " + str(result2) + "."
+    return result
+
+
+def parse_corrupted_data(data, enable: bool = False):
+    result = 0
+    active = True
+    operations = []
+    for line in data:
+        operations.extend(re.findall("(?:mul\(\d+\,\d+\)|don't\(\)|do\(\))", line))
+    for operation in operations:
+        if "mul" in operation and active:
+            values = re.findall("\d+", operation)
+            result += int(values[0]) * int(values[1])
+        if operation == "don't()" and enable:
+            active = False
+        if operation == "do()" and enable:
+            active = True
+    return result
